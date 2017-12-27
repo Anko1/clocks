2ade2e32b36265787693da139dc441751111bbb7
Anko1 <fortuna.me@ukr.net>
2017-12-27 21:01:56 +0200

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+$(function () {
+	$('.multiple-items').slick({
+								   infinite: true,
+								   slidesToShow: 3,
+								   slidesToScroll: 1,
+								   prevArrow: $('#prev'),
+								   nextArrow: $('#next'),
+								   // variableWidth: true
+							   });
+});
+
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const PORT = 8888;
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set('view engine', 'hbs');
+
+server.get('/', (req, res) => {
+	res.render(__dirname + '/index.hbs', {
+											 logo: 'LogoName'
+										 });
+});
+
+server.get('/clock/:id', (req, res) => {
+	res.render(__dirname + '/clock.hbs', {
+											 id: req.params.id
+										 });
+});
+
+server.listen(PORT, () => {console.log('Sever started!')});
